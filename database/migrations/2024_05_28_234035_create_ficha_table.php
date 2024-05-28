22ffff97bac217e20cd9466ec70bcbb1ae7ebee1
Tarcisio-Souto <tss.labsi@gmail.com>
2024-05-28 20:48:10 -0300

    criando models e migrations

---
+++ database/migrations/2024_05_28_234035_create_ficha_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ficha', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('dtvisita');
+            $table->unsignedBigInteger('paciente_id');
+            $table->foreign('paciente_id')->references('id')->on('paciente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ficha');
+    }
+};
